e11afeb9c160af03a8583db12451187cf6c3f539
Iamneo <techadmin@iamneo.ai>
2023-08-07 11:01:55 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -6,46 +6,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
+import java.net.MalformedURLException;
 
 public class SpringApplicationTests {
 
 	public static void main(String[] args) {
+		try{
 		String url = "http://www.flipkart.com";
-    ChromeOptions chromeOptions = new ChromeOptions();
-	WebDriver driver = null;
-	}
-	
+		ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new RemoteWebDriver(new URL("http://34.85.242.216:4444"), chromeOptions);
+		driver.manage().window().maximize();
+		driver.navigate().to("http://www.flipkart.com");
+		String title = driver.getTitle();
+		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//input[@type='text']"))
+				.sendKeys("testuser@abc.com");
 
-	@BeforeTest
-	public void beforeTest() throws Exception
-	 {
-		driver = new RemoteWebDriver(new URL("http://34.85.242.216:4444/"), chromeOptions);
-	    driver.manage().window().maximize();
-	}
-	@Test
-    public void Facebook() throws InterruptedException 
-	{
-        driver.navigate().to("http://www.flipkart.com");
-		String title =driver.getTitle(); 
-	}
-    @Test
-	public void loginToFlipkart() {
-		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//input[@type='text']")).sendKeys("testuser@abc.com");
-		
-		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//input[@type='password']")).sendKeys("Test@1234");
-		
+		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//input[@type='password']"))
+				.sendKeys("Test@1234");
+
 		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//button[@type='submit']")).click();
-		
-	}
-
-	@AfterTest
-	public void closeBrowser() {
 		driver.quit();
+		}catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
